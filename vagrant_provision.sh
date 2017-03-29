646a7ad5dc25ac24615dfbbdda5df779698f0e02
cgarcia <carlos.garcia@stoneworksolutions.net>
2017-03-29 12:40:43 +0200

    Voy a llorar

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -28,7 +28,7 @@ EOF
 cat << EOF > $HOME/.bash_aliases
 # dev avoids annoying bug in sendfile syscall
 alias dev="sudo sh -c 'echo 1 > /proc/sys/vm/drop_caches'; docker-compose -f docker-compose.yml"
-alias python="dev exec web python"
+
 alias manage="python manage.py"
 alias shell="flask shell"
 alias clean="docker images --no-trunc | grep '<none>' | awk '{ print \$3 }' | xargs -r docker rmi ; docker ps --filter status=dead --filter status=exited -aq  | xargs docker rm -v ; docker volume ls -qf dangling=true | xargs -r docker volume rm"
